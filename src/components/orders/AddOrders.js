6f3ce2f3695fca0309f1f3cb79a44c543e13a757
Rahul7044 <rahulkumar581999@gmail.com>
2023-04-07 03:42:59 +0530

    formcreated-for adding-items

---
+++ src/components/orders/AddOrders.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const AddOrders = () =>{
+    <div>
+        <form>
+            <label htmlFor="uniqueID">Unique ID</label>
+            <input type="number" id="uniqueID"></input>
+            <label htmlFor="price">Unique ID</label>
+            <input type="number" id="price"></input>
+            <label htmlFor="uniqueID">Unique ID</label>
+            <input type="text" id="dish"></input>
+        </form>
+    </div>
+
+};
+export default AddOrders;
